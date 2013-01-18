195c1936a9e0c63b21006822567713485fc9150f
AJ Morris <ajmorris2002@gmail.com>
2013-01-18 17:58:45 -0500

    A lot is looking now. i've kind of forgot to do some committing since I last did.

--- functions.php
+++ functions.php
@@ -1,4 +1,18 @@
 <?php
-
+    
+    /**
+     * Initializer of theme functions
+     * to run on init
+     * @return [type]
+     */
+    function theme_initializer() {
+        /*
+        Register header menu
+         */
+        register_nav_menus(
+            array( 'header-menu' => __( 'Header Menu' ) )
+        );
+    }
+    add_action( 'init', 'theme_initializer' );
 
 ?>
--- header.php
+++ header.php
@@ -6,12 +6,12 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
+        <title><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-
+        <link rel="stylesheet" href="<?php bloginfo(template_directory); ?>/css/app.css">
         <!-- Need to include this, but through the use of require or background or something like that. -->
         <!-- <script src="js/vendor/modernizr-2.6.2.min.js"></script> -->
     </head>
@@ -19,3 +19,19 @@
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
+
+        <header>
+            <div class="header">
+                <nav>
+                    <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'top-nav' ) ); ?>
+                </nav>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="twelve columns logo">
+                        <span class="blog-title"><?php echo bloginfo('name'); ?></span>    
+                        <span class="blog-description"><?php echo bloginfo('description'); ?></span>
+                    </div>
+                </div> 
+            </div>
+        </header>
