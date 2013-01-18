bbeb294b8bca4eb7c99d2a2fddb2b19faac0becd
AJ Morris <ajmorris2002@gmail.com>
2013-01-18 10:59:18 -0500

    Updated the header.php file

--- header.php
+++ header.php
@@ -12,9 +12,8 @@
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <!-- Need to include this, but through the use of require or background or something like that. -->
+        <!-- <script src="js/vendor/modernizr-2.6.2.min.js"></script> -->
     </head>
     <body>
         <!--[if lt IE 7]>
